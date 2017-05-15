4a284b5ae424dc74ecf13636061e27551fe39712
Jonathan Arias <Jonathan-Arias@users.noreply.github.com>
2017-05-14 21:11:08 -0700

    Moved binocdf to function file

--- functions.js
+++ functions.js
@@ -1,3 +1,74 @@
+/* Binomial Distribution Function Calculator
+ * Author: Thomas Ferguson
+ * Source: http://www.math.ucla.edu/~tom/distributions/binomial.html
+ */
+
+function LogGamma(Z) {
+    with(Math) {
+        var S = 1 + 76.18009173 / Z - 86.50532033 / (Z + 1) + 24.01409822 / (Z + 2) - 1.231739516 / (Z + 3) + .00120858003 / (Z + 4) - .00000536382 / (Z + 5);
+        var LG = (Z - .5) * log(Z + 4.5) - (Z + 4.5) + log(S * 2.50662827465);
+    }
+    return LG
+}
+
+function Betinc(X, A, B) {
+    var A0 = 0;
+    var B0 = 1;
+    var A1 = 1;
+    var B1 = 1;
+    var M9 = 0;
+    var A2 = 0;
+    var C9;
+    while (Math.abs((A1 - A2) / A1) > .00001) {
+        A2 = A1;
+        C9 = -(A + M9) * (A + B + M9) * X / (A + 2 * M9) / (A + 2 * M9 + 1);
+        A0 = A1 + C9 * A0;
+        B0 = B1 + C9 * B0;
+        M9 = M9 + 1;
+        C9 = M9 * (B - M9) * X / (A + 2 * M9 - 1) / (A + 2 * M9);
+        A1 = A0 + C9 * A1;
+        B1 = B0 + C9 * B1;
+        A0 = A0 / B1;
+        B0 = B0 / B1;
+        A1 = A1 / B1;
+        B1 = 1;
+    }
+    return A1 / A
+}
+
+function compute(argument, samplesize, prob) {
+    X = argument;
+    N = samplesize;
+    P = prob;
+    with(Math) {
+        if (N <= 0) {
+            alert("sample size must be positive")
+        } else if ((P < 0) || (P > 1)) {
+            alert("probability must be between 0 and 1")
+        } else if (X < 0) {
+            bincdf = 0
+        } else if (X >= N) {
+            bincdf = 1
+        } else {
+            X = floor(X);
+            Z = P;
+            A = X + 1;
+            B = N - X;
+            S = A + B;
+            BT = exp(LogGamma(S) - LogGamma(B) - LogGamma(A) + A * log(Z) + B * log(1 - Z));
+            if (Z < (A + 1) / (S + 2)) {
+                Betacdf = BT * Betinc(Z, A, B)
+            } else {
+                Betacdf = 1 - BT * Betinc(1 - Z, B, A)
+            }
+            bincdf = 1 - Betacdf;
+        }
+        bincdf = round(bincdf * 100000) / 100000;
+    }
+    return bincdf;
+}
+
+
 // function to check for participant consent
 var check_consent = function(elem) {
     if ($('#consent_checkbox').is(':checked')) {
@@ -234,6 +305,9 @@ function predictNextPlay() {
     var N = 20;
     var choices = [];
 
+    var nPossibleEntries = 3;
+    var alphaLevel = 1/nPossibleEntries;
+
     for (var k = -1; k > -20; k--)
     {
         var re = new RegExp(str.substr(k),'g');
@@ -264,6 +338,32 @@ function predictNextPlay() {
             if (x == 3) scs++;
         }
         choices.push([rks,ppr,scs]);
-        console.log(choices);
     }
+
+    var pVals = [];
+    var mins;
+    var choice;
+    var X, N, P;
+    var bcf;
+    for (var i = 0; i < choices.length; i++){
+        choice = choices[i];
+        X = Math.max(...choice);
+        N = choice[0] + choice[1] + choice[2];
+        P = 1/nPossibleEntries;
+        bcf = compute(X,N,P);
+        pVals[i] = 1 - bcf;
+    }
+
+    var predictConfidence = Math.min(...pVals);
+    if (predictConfidence >= alphaLevel)
+    {
+        return computerRandomMove();
+    } else 
+    {
+        var ind = pVals.indexOf(predictConfidence);
+        var nextItem = Math.max(...choices[ind]);
+    }
+
+    
+    return nextItem;
 }
